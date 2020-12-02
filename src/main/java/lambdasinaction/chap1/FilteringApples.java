f8e1e7bc8fc076eb2062205275c07208f3bca9bd
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2020-12-02 10:15:47 +0800

    chap 2

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -9,30 +9,31 @@ public class FilteringApples{
 
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                                               new Apple(155, "green"),
-                                              new Apple(120, "red"));	
+                                              new Apple(120, "red"));
 
+
+        /** 通过java8支持传递代码值的特性，消除了老java的重复代码带来的危害。 **/
         // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
         System.out.println(greenApples);
-        
         // [Apple{color='green', weight=155}]
         List<Apple> heavyApples = filterApples(inventory, FilteringApples::isHeavyApple);
         System.out.println(heavyApples);
-        
+
+        /** 以下用lambda，更简单，连短方法都不用定义了。 **/
         // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         List<Apple> greenApples2 = filterApples(inventory, (Apple a) -> "green".equals(a.getColor()));
         System.out.println(greenApples2);
-        
         // [Apple{color='green', weight=155}]
         List<Apple> heavyApples2 = filterApples(inventory, (Apple a) -> a.getWeight() > 150);
         System.out.println(heavyApples2);
-        
         // []
         List<Apple> weirdApples = filterApples(inventory, (Apple a) -> a.getWeight() < 80 || 
                                                                        "brown".equals(a.getColor()));
         System.out.println(weirdApples);
     }
 
+    //以下是java8之前的写法，最啰嗦，有重复代码就易错。
     public static List<Apple> filterGreenApples(List<Apple> inventory){
         List<Apple> result = new ArrayList<>();
         for (Apple apple: inventory){
@@ -42,7 +43,6 @@ public class FilteringApples{
         }
         return result;
     }
-
     public static List<Apple> filterHeavyApples(List<Apple> inventory){
         List<Apple> result = new ArrayList<>();
         for (Apple apple: inventory){
